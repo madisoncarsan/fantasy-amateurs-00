edef39339cc08cb9fe0d7d1fd802f0dcd9f07c60
madisoncarsan <69589527+madisoncarsan@users.noreply.github.com>
2022-01-05 10:00:18 -0800

    Add files via upload

--- FF_Python_Test.py
+++ FF_Python_Test.py
@@ -3,10 +3,10 @@
 # Football Import
 from espn_api.football import League
 
-#League Information - Includes login with espn_s2 and swid (needed since it is a private league)
+# League Information - Includes login with espn_s2 and swid (needed since it is a private league)
 league = League(league_id=51591979, year=2021, espn_s2='AEBjEqbBxjZIt45ocr9eFgbp%2FBkWyQmzCIuNSsyI4jmQqhhxc2s5mnRJxHr5QOc576BaF1ULvFqeeTZoM%2FHg%2FS%2FHS1beH7XX8aP4uLisyHbr1SkANX42Es7E%2BZuq%2FXqKmSDMSVTYTMF3ctf%2FKmVMapTGJNkADWmsEoTHoIT8QTdj80aENnYeygi7DGqIDjrcCbLVfcWxUWp3jKqxQRP1744e88mki7wkLuNydGGonvYuPlrfcsDCEFY%2FVpkoq9lc%2FKm0mBdoWAiWsMeb7B3w5c25',swid='{7EDF6E52-328E-4864-9E53-6190364DDF91}')
 
-#Dictionary so that it is easy to reference each team by member name
+# Dictionary so that it is easy to reference each team by member name
 Team_Dictionary = {
 	
 	"Madison": league.teams[0],
@@ -23,9 +23,48 @@ Team_Dictionary = {
 	"Mitch": league.teams[11],
 }
 
+# Basic Tests 
+
+#///////////
+
 # print (Team_Dictionary["Madison"])
 # print (league.teams)
+# print (Team_Dictionary["Madison"].roster[2].name)
 
-print (Team_Dictionary["Madison"].roster[2].name)
+#///////////////
+
+# Trade pulling
+trades = league.recent_activity(msg_type='TRADED')
+
+print (trades[0].actions[0])
+
+
+# This pulls the last trade that was executed (list gets prepended, so the latest one is [0])
+# print (trades[0])
+
+# This generates single line items for each player being traded in a trade
+# for action in trades[0].actions:
+	# print (action)
+
+
+# Separating trades into what players each member is sending
+
+#for action in trades[0].actions:
+
+	# Print team name of the line item
+	# print (action[0].team_name)
+
+	# print player being sent in transaction
+	# print (action[2].name)
+
+
+# for action in trades[0].actions:
+	#if 
+
+	# Need to create a team class for this
+
+
+
+
 
 
