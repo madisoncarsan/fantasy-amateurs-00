403756aca9dc91018e02ebd0e8da534b7392cafd
madisoncarsan <69589527+madisoncarsan@users.noreply.github.com>
2022-01-05 15:38:18 -0800

    Add files via upload

---
+++ Team_Class.py
@@ -0,0 +1,10 @@
+"""
+Class for a team so that we can store player information within them
+"""
+
+class Team_Class:
+	def __init__(self):
+
+		self.Member = None
+		self.Team_Name = None
+		self.Players = []
